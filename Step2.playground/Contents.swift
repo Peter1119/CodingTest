819918dcdd6b45d4ce938adf4290e987eb9a0993
nyokki1119 <ghdtjrgus333@naver.com>
2021-06-24 18:14:31 +0900

    if, 사칙연산

---
+++ Step2.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+//1330번
+/*
+ 문제
+ 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+ 
+ 입력
+ 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+ 
+ 출력
+ 첫째 줄에 다음 세 가지 중 하나를 출력한다.
+ 
+ A가 B보다 큰 경우에는 '>'를 출력한다.
+ A가 B보다 작은 경우에는 '<'를 출력한다.
+ A와 B가 같은 경우에는 '=='를 출력한다.
+ 제한
+ -10,000 ≤ A, B ≤ 10,000
+ */
+let line = readLine()!
+let lineArr = line.split(separator: " ")
+
+let A = Int(lineArr[0]) ?? 0
+let B = Int(lineArr[1]) ?? 0
+
+if A < B {
+    print("<")
+} else if A > B {
+    print(">")
+} else {
+    print("==")
+}
+
+//
---
+++ step1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+for _ in 1...2 {
+    print("강한친구 대한육군")
+}
+
+let test = #"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#
+print(test)
+
+
